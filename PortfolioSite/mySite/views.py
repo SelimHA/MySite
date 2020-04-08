b8e638d9ab89df407222b7102d785f877e1df458
Selim Hadjadj-Aoul <szelmoca@gmail.com>
2020-04-08 21:26:57 +0100

    Pagination added, code fixed at filtering

--- PortfolioSite/mySite/views.py
+++ PortfolioSite/mySite/views.py
@@ -4,13 +4,18 @@ from django.http import HttpResponse
 from django.conf import settings
 from mySite.models import projectImage, Project
 from mySite.GitGetter import *
+from django.core.paginator import Paginator
 # Create your views here.
 def index(request):
     context_dict = {"languages": getTotalLanguages()}
     return render(request, 'index.html', context_dict) 
 
 def repo(request):
-    context_dict = {"project_data": getAllProjectDetails()}
+    data = getAllProjectDetails()
+    paginator = Paginator(data, 1)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+    context_dict ={"project_data": page_obj}
     return render(request, 'mySite/repo.html', context_dict)
 
 def curprojects(request):
@@ -21,7 +26,7 @@ def curprojects(request):
         curProject["name"] = project.title
         curProject["details"] = project.text
         curProject["language_icon"]= getIcon(project.language)
-       # images = projectImage.objects.all().filter(title=curProject["name"])
-        #curProject["images"]=images
+        images = projectImage.objects.all().filter(title=project)
+        curProject["images"]=images
         context_dict["project"].append(curProject)
     return render(request, 'mySite/current_projects.html', context_dict)
