3e2b51fd6f96e181ff04f0290e81eb2ba98111d9
Selim Hadjadj-Aoul <szelmoca@gmail.com>
2020-04-22 16:41:06 +0100

    Bugfixes

--- PortfolioSite/mySite/urls.py
+++ PortfolioSite/mySite/urls.py
@@ -2,10 +2,11 @@ from django.urls import path
 from django.urls import include
 from django.conf import settings
 from mySite import views
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('', views.index, name='index'),
     path('about', views.about, name='about'),
     path('repo', views.repo, name='repo'),
     path('current_projects', views.curprojects, name='current_projects'),
-]
+]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
